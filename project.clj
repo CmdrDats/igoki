fa609adf81cc8e55025386b1b128ef01a13e5795
Deon Moolman <deon@yuppiechef.com>
2015-10-29 08:36:57 +0200

    Update opencv dependency - opencv 2.4.11 + windows binaries!

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [[org.clojure/clojure "1.7.0"]
-   [nu.pattern/opencv "2.4.9-7"]
+   [org.openpnp/opencv "2.4.11-1"]
    [quil "2.2.6"]
    [clj-http "2.0.0"]
    [cheshire "5.5.0"]]
