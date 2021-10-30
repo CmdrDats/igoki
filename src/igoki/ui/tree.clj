3c3d57395e7b6ec86ae7ac0e33b2dac2beeb131f
Deon Moolman <cmdrdats@gmail.com>
2021-10-30 20:54:29 +0200

    super simple basic bare-bones tree implemented
    (needs a _lot_ of work.. but this is a start.)

--- src/igoki/ui/tree.clj
+++ src/igoki/ui/tree.clj
@@ -3,64 +3,84 @@
     [seesaw.core :as s]
     [seesaw.border :as sb]
     [clojure.pprint :as ppr]
-    [clojure.tools.logging :as log]))
+    [clojure.tools.logging :as log]
+    [seesaw.color :as sc])
+  (:import (java.awt Graphics2D)))
 
 
-#_(defn prerender-branch
-  ([node]
-   (prerender-branch nil
-     {:x 0
-      :y 0
-      :width 7
-      :height 10
-      :colour :red}
-     node))
-  ([parent node]
-   (cond
-     (nil? node) nil
-
-
-     )
-   (let [basenode
-         {:x (+ (:x parent) (:width parent))
-          :y (:y parent)
-          :width 7 :height 10
+(defn prerender-branch
+  ([idx node]
+   (println idx)
+   (let [n
+         {:x (+ 10 (* idx 7))
+          :y (or (:y node) 10)
+          :width 7
+          :height 10
           :colour :red}
 
-         root
-         (cond
-           (nil? node)
-           nil
+         branches
+         (reduce
+           (fn [acc b]
+             (let [r
+                   (prerender-branch (inc idx)
+                     (if (> idx 500)
+                       (->
+                         b
+                         (assoc :y (:y acc))
+                         (dissoc :branches))
 
-           (:black node)
-           (assoc basenode :colour :black)
-
-
-           (:white node)
-           (assoc basenode :colour :white)
-
-           :else
-           basenode)
-
-         children
-         (map-indexed
-           (fn [idx child]
-             (prerender-branch (update root :y + (* idx 10)) child))
+                       (->
+                         b
+                         (assoc :y (:y acc)))))]
+               (->
+                 acc
+                 (update :height + 2 (:height r))
+                 (update :width max (:width r))
+                 (update :y + 2 (:height r))
+                 (update :nodes conj r))))
+           {:y (:y n)
+            :height 0
+            :width 0
+            :nodes []}
            (:branches node))]
-     (assoc root
-       :width (max ))
-     )))
+
+     #_(println (:y branches))
+     (assoc n
+       :height (max (:height branches) (:height n))
+       :width (+ (:width branches) (:width n))
+       :branches (when-not (empty? (:nodes branches)) (:nodes branches))
+       :idx idx
+       :colour
+       (cond
+         (:black node) :black
+         (:white node) :white
+         :else :red)))))
+
+#_(defn node-labels [{:keys [x y width height branches colour]}]
+  (concat
+    [(s/label
+       :background colour :border (sb/line-border :color :black :thickness 1)
+       :text " " :bounds [x y 7 10])]
+    (mapcat node-labels branches)))
+
+(defn paint-tree [ctx node c ^Graphics2D g]
+  (.setColor g (sc/color (:colour node)))
+  (.fillRect g (:x node) (:y node) 7 10)
+  (.setColor g (sc/color :black))
+  (.drawLine g (:x node) (:y node) (+ 7 (:x node)) (:y node))
+  (.drawLine g (:x node) (+ 10 (:y node)) (+ 7 (:x node)) (+ 10 (:y node)))
+  (doseq [n (:branches node)]
+    (paint-tree ctx n c g)))
 
 (defn rendered-tree-panel [ctx]
-  (println "hello?")
   #_(s/flow-panel :id :rendered-tree
     :items [(s/label :text "hi" :bounds [10 10 100 100])
             (s/button :text "fdsafdsafdfd")])
-  (let [#_#_prerendered (prerender-branch nil (get-in @ctx [:kifu :moves]))
+  (let [prerendered (prerender-branch 0 (get-in @ctx [:kifu :moves]))
         xyz
-        (s/xyz-panel
-          :size [1100 :by 100]
-          :items [(s/label :background :white :border (sb/line-border :color :black :thickness 1) :text "hi" :bounds [1000 10 100 100])])]
+        (s/canvas
+          :paint (partial #'paint-tree ctx prerendered)
+          :size [(:width prerendered) :by (:height prerendered)])]
     (s/scrollable
       xyz
       :id :rendered-tree
